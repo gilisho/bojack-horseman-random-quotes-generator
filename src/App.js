27efaa96114a46fcf23735eb2c956b31faabce59
g101 <29674796+gilisho@users.noreply.github.com>
2018-04-06 16:35:36 +0300

    Add features
    Things that were added:
    1. quotes
    2. font
    3. button to generate quote
    3. credits section
    
    Updated style of website.

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ class App extends Component {
   constructor() {
     super();
     this.quotes = [
+      "I'm supposed to be responsible for my own happiness? I can't even be responsible for my own breakfast!",
+      "There's nothing the least bit funny about stealing a meal from Neal McBeal the Navy Seal.",
       "Would you like an alcohol?",
-      "I did a business",
+      "I did a business.",
       "That's too much, man!",
       "It gets easier. Everyday it gets a little easier. But you gotta do it everyday, that's the hard part. But it does get easier.",
       "When you look at someone through rose-colored glasses, all the red flags just look like flags.",
       "You know, sometimes I feel like I was born with a leak, and any goodness I started with just slowly spilled out of me, and now it's all gone. And I'll never get it back in me. It's too late.",
-      "I don't understand how people live. It's amazing to me that people wake up every morning and say \"Yeah! Another day! Let's do it!!\" How do people do it? I don't know how.",
+      "I don't understand how people live. It's amazing to me that people wake up every morning and say \"Yeah! Another day! Let's do it!\" How do people do it? I don't know how.",
       "Kelsey, in this terrifying world, all we have are the connections that we make.",
       "I do love you by the way, I mean as much as I'm capable of loving anyone, which is never enough.",
-      "See, Sarah Lynn? We're not doomed. In the great grand scheme of things, we're just tiny specks that will one day be forgotten. So it doesn't matter what we did in the past or how we'll be remembered, the only thing that matters is right now, this moment. This one spectacular moment we are sharing together. Right, Sarah Lynn?",
+      "See, Sarah Lynn? We're not doomed. In the great grand scheme of things, we're just tiny specks that will one day be forgotten. So it doesn't matter what we did in the past or how we'll be remembered, the only thing that matters is right now, this moment. This one spectacular moment we are sharing together. Right, Sarah Lynn? ...Sarah Lynn? ...Sarah Lynn?",
       "Believe it or not, time's arrow neither stands still nor reverses. It merely marches forward.",
       "You know what I do when I have a really bad, awful, terrible day? I imagine my great-great-great granddaughter in the future talking to her class about me. She's poised and funny, and tells people about me and how everything worked out in the end. And when I think about that, I think about how everything's going to work out. Because how else could she tell people?",
       "I got into this business because I love stories. They comfort us, they inspire us, they create a context for how we experience the world. But also, you have to be careful, because if you spend a lot of time with stories, you start to believe that life is just stories, and it's not. Life is life, and that's so sad, because there's so little time and... what are we doing with it?",
@@ -29,10 +31,10 @@ class App extends Component {
       "I believe it was Benjamin Franklin who said: \"You have reached the end of you free trial membership at BenjaminFranklinQuotes.com.\"",
       "Fool me once shame on me, but teach a man to fool me and I'll be fooled for the rest of my life.",
       "Fool me once, fool me twice, fool me chicken soup and rice.",
-      "I'm more of a before rehab kind of friend",
+      "I'm more of a before rehab kind of friend.",
       "It's so cruel to let people love you. All you're doing is promising you'll one day break their hearts.",
       "Only after you give up everything can you begin to find a way to be happy.",
-      "Don't fetishize your own sadness"
+      "Don't fetishize your own sadness."
     ];
     this.mainButtonClickHandler = this.mainButtonClickHandler.bind(this);
     this.volumeClickHandler = this.volumeClickHandler.bind(this);
@@ -64,23 +66,22 @@ class App extends Component {
     return (
       <div className="app-root">
         <audio autoPlay loop muted={this.state.mute}>
-          <source src={music} type="audio/mpeg">
-          </source>
+          <source src={music} type="audio/mpeg"></source>
         </audio>
         <div className="quotes-header">
           <h2>Bojack Horseman Random Quote Generator</h2>
           <p className="quote-text">{this.state.currQuote}</p>
           <button className="button" onClick={this.mainButtonClickHandler}>Get Another Quote!</button>
-          <FontAwesome
-            className="volume-button"
-            name={(this.state.mute) ? "volume-off" : "volume-up"}
-            title={(this.state.mute) ? "unmute" : "mute"}
-            onClick={this.volumeClickHandler}
-          />
-
         </div>
-        <p className="credits">Credit to the talented <a href="http://www.pandatooth.com/">PandaTooth</a> for the music!</p>
-
+        <FontAwesome
+          className="volume-button"
+          name={(this.state.mute) ? "volume-off" : "volume-up"}
+          title={(this.state.mute) ? "unmute" : "mute"}
+          onClick={this.volumeClickHandler}
+        />
+        <div className="credits">
+          Credit to the talented <a href="http://www.pandatooth.com" target="_blank"/*open in new tab*/>PandaTooth</a> for the music
+          and to <a href="http://www.bythebutterfly.com" target="_blank">bythebutterfly</a> for the font!</div>
       </div>
     );
   }
